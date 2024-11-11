7faa4fcbca3e41d244d3709cb9b9834499b9c4a6
yuanfutong <yuanfutong@kingsoft.com>
2024-11-11 20:01:29 +0800

    Add: 配置 element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import fs from 'node:fs'
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import electron from 'vite-plugin-electron/simple'
 import pkg from './package.json'
 
-// https://vitejs.dev/config/
+const pathSrc = path.resolve(__dirname, 'src')
+
+
 export default defineConfig(({ command }) => {
   fs.rmSync('dist-electron', { recursive: true, force: true })
 
@@ -13,8 +21,42 @@ export default defineConfig(({ command }) => {
   const sourcemap = isServe || !!process.env.VSCODE_DEBUG
 
   return {
+
+    resolve: {
+      alias: {
+        '@': pathSrc,
+      },
+    },
+
     plugins: [
       vue(),
+      Components({
+        resolvers: [
+          // 自动注册图标组件
+          IconsResolver({
+            enabledCollections: ['ep'],
+          }),
+          ElementPlusResolver({
+            importStyle: 'css'
+          })
+        ],
+        dts: path.resolve(pathSrc, 'components.d.ts'), // 指定自动导入函数TS类型声明文件路径
+        dirs: ["./src/components/**"], // 自动引入'./src/components/'中的所有组件
+      }),
+      AutoImport({
+        imports: ['vue', 'vue-router'],
+        resolvers: [
+          // 自动导入图标组件
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+          ElementPlusResolver({
+            importStyle: 'css'
+          })
+        ],
+        dts: path.resolve(pathSrc, 'auto-imports.d.ts'), // 指定自动导入函数TS类型声明文件路径
+        // dirs: ["./src/utils/**"], // utils下的所有文件都支持自动引入
+      }),
       electron({
         main: {
           // Shortcut of `build.lib.entry`
@@ -32,7 +74,7 @@ export default defineConfig(({ command }) => {
               minify: isBuild,
               outDir: 'dist-electron/main',
               rollupOptions: {
-                // Some third-party Node.js libraries may not be built correctly by Vite, especially `C/C++` addons, 
+                // Some third-party Node.js libraries may not be built correctly by Vite, especially `C/C++` addons,
                 // we can use `external` to exclude them to ensure they work correctly.
                 // Others need to put them in `dependencies` to ensure they are collected into `app.asar` after the app is built.
                 // Of course, this is not absolute, just this way is relatively simple. :)
